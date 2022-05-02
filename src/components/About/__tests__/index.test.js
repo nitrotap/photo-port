35757628eae29602db4fbd96aea3f3d622ecb0a3
nitrotap <kartikinpublic@gmail.com>
2022-05-02 16:08:22 -0600

    adding gallery component

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,24 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
 import About from '..';
-const { asFragment } = render(<About />);
-
 
 afterEach(cleanup);
 
-
-describe('About component', () => {
-    //renders About test
-    it('renders', () => {
-        render(<About />);
-    });
-
-    it('matches snapshot DOM node structure', () => {
-        // render About
-        expect(asFragment()).toMatchSnapshot();
-
-    })
-
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
